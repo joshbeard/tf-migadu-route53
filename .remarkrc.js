fbd02cbba2d031a1ad43b03da8fc0fdabfea73b5
Josh Beard <josh@joshbeard.me>
2022-08-02 17:37:26 -0600

    linting: add remark rules; ignore empty line check

--- .remarkrc.js
+++ .remarkrc.js
@@ -8,6 +8,7 @@ exports.plugins = [
     ['remark-lint-list-item-indent', false],
     ['remark-lint-no-dead-urls', false], // Does not work because of github protection system
     ['remark-lint-maximum-line-length', 120],
+    ['remark-lint-no-consecutive-blank-lines', false], // Complains about the ending empty line
     ['remark-lint-maximum-heading-length', 120],
     ['remark-lint-strong-marker', '*'],
     ['remark-lint-emphasis-marker', '_'],
